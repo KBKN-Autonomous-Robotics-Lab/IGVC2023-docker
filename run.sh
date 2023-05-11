ad3ca141b8c5c776984ab4f53e274f993ad684a5
kengohozumi <kengo.hozumi.5n@stu.hosei.ac.jp>
2023-05-11 07:42:19 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -9,15 +9,15 @@ docker run \
 	--shm-size=512m \
 	--entrypoint '/startup.sh' \
 	--device /dev/ZLAC8015D:/dev/ZLAC8015D:mwr \
-	--device /dev/sensors/hokuyo_urg:/dev/sensors/hokuyo_urg:mwr \
 	--device /dev/sensors/imu:/dev/sensors/imu:mwr \
 	--device /dev/sensors/insta360_air:/dev/sensors/insta360_air:mwr \
 	--device /dev/input/js0:/dev/input/js0:mwr \
 	--device /dev/sensors/CLAS:/dev/sensors/CLAS:mwr \
 	--device /dev/sensors/GNSSbase:/dev/sensors/GNSSbase:mwr \
 	--device /dev/sensors/GNSSrover:/dev/sensors/GNSSrover:mwr \
-	--device /dev/E-Stop:/dev/E-Stop:mwr \
 	--device /dev/sensors/LED:/dev/sensors/LED:mwr \
 	igvc2023
 	
 	#-e RESOLUTION=1920x1080
+	#--device /dev/E-Stop:/dev/E-Stop:mwr \
+	#--device /dev/sensors/hokuyo_urg:/dev/sensors/hokuyo_urg:mwr \
